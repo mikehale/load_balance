5315dbd368516c481ad4f8b1714c35163ac3975a
Michael Hale <mikehale@gmail.com>
2009-09-01 23:04:56 -0400

    add cluster ip configs

--- config/dna.rb
+++ config/dna.rb
@@ -2,14 +2,11 @@ require 'rubygems'
 require 'json'
 
 dna = {
-  # :keepalived => {:servers => %w[172.16.209.129 172.16.209.130] },
-  # :recipes => [
-  #       "keepalived"
-  #      ]
-  :heartbeat => {:servers => %w[vm-ubuntu-1 vm-ubuntu-2],
-                 :service_address => "172.16.209.200",
+  :cluster => {:members => %w[vm-ubuntu-1 vm-ubuntu-2],
+                 :ip => "172.16.209.200",
+                 :mac => "01:02:03:04:05:06"
                  :secret => "whateverdude"},
-  :recipes => %w[heartbeat]
+  :recipes => %w[clusterip]
 }
 
 open(File.dirname(__FILE__) + "/dna.json", "w").write(dna.to_json)
---
+++ site-cookbooks/clusterip/recipes/default.rb
@@ -0,0 +1,21 @@
+service "heartbeat" do
+  action :stop
+end
+
+bash "install modules" do
+  code "modprobe ipt_CLUSTERIP && modprobe ipt_conntrack"
+end
+
+bash "add clusterip rule to iptables" do
+  local_node = node[:cluster][:members].index(node[:hostname]) + 1
+  total_nodes = node[:cluster][:members].size
+  
+  code %(iptables -F INPUT && 
+         iptables -I INPUT -d #{node[:cluster][:ip]} -i eth0 -j CLUSTERIP \
+            --new --clustermac #{node[:cluster][:mac]} --total-nodes #{total_nodes} \
+            --local-node #{local_node} --hashmode sourceip --hash-init 5567)
+end
+
+bash "add shared ip to interface" do
+  code "ip address add #{node[:shared_ip]} dev eth0 || true"
+end
